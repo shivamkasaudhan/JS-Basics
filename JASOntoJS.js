b36ae400fc0827e05ae693ee1f7af78c24c46ef5
SHIVAM KUMAR KASAUDHAN <124242999+shivamkasaudhan@users.noreply.github.com>
2023-09-22 17:10:36 +0530

    json to js, js to json

---
+++ JASOntoJS.js
@@ -0,0 +1,13 @@
+const jsonStr = '{"color": "red"}';
+
+const jsStr = JSON.parse(jsonStr);
+console.log(jsStr)
+
+
+const data = {
+    first: "shivam",
+    second: "kumar",
+    third: "kasaudhan"
+}
+const jsonData = JSON.stringify(data)
+console.log(jsonData)
